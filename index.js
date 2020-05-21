5aec165d0cc8f12c7a7f204569b3de5593ee144c
Risto Nishimura <risto24.contact@gmail.com>
2020-05-21 07:14:22 +0000

    tweak

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const str = 'aaaa https://qiita.com aaa';
 
 const makeLink = (str) => {
-    let regexp_url = /((h?)(ttps?:\/\/[a-zA-Z0-9.\-_@:/~?%&;=+#',()*!]+))/g; // ']))/;
+    let regexp_url = /((h?)(ttps?:\/\/[a-zA-Z0-9.\-_@:/~?%&;=+#',()*!]+))/g;
     let regexp_makeLink = function(all, url, h, href) {
-        return '<a href="h' + href + '">' + url + '</a>';
+        return `h${href}`;
     }
     return str.replace(regexp_url, regexp_makeLink);
+    // if you want only urls
+    // return str.match(regexp_url);
 }
 
 makeLink(str);
